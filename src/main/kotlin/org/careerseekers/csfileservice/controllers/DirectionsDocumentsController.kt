b2c3aa7172a9e729700ff1803aacbc98e42a45eb
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-09 21:25:38 +0300

    feat(api): created endpoint for loading direction documents

---
+++ src/main/kotlin/org/careerseekers/csfileservice/controllers/DirectionsDocumentsController.kt
@@ -0,0 +1,25 @@
+package org.careerseekers.csfileservice.controllers
+
+import org.careerseekers.csfileservice.entities.FilesStorage
+import org.careerseekers.csfileservice.enums.FileTypes
+import org.careerseekers.csfileservice.services.FileService
+import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@RestController
+@RequestMapping("/file-service/v1/files")
+class DirectionsDocumentsController(private val fileService: FileService) {
+
+    @PostMapping("/uploadDirectionsDocument", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun loadDocument(
+        @RequestPart("file") file: FilePart,
+        @RequestPart("type") type: String,
+    ): Mono<FilesStorage> {
+        return fileService.saveFile(file, FileTypes.valueOf(type.uppercase()))
+    }
+}
